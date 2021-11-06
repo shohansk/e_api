f17f19b9c6f5ecffd91def3d46077af1a61e1094
shohansk <shohankhansk111@gmail.com>
2021-11-06 23:19:30 +0600

    done

--- EcommerceApi/models.py
+++ EcommerceApi/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     tittle = models.CharField(max_length = 255)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.tittle
 
